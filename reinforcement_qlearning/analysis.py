665c18b45e49785d1dcadbced0c5aae01583af9f
Mikhail Skobov <mskobov@lyft.com>
2019-02-16 15:35:02 -0800

    Adding answer for q8

--- reinforcement_qlearning/analysis.py
+++ reinforcement_qlearning/analysis.py
@@ -19,7 +19,8 @@
 def question8():
     answerEpsilon = None
     answerLearningRate = None
-    return answerEpsilon, answerLearningRate
+    #return answerEpsilon, answerLearningRate
+    return 'NOT POSSIBLE'
     # If not possible, return 'NOT POSSIBLE'
 
 if __name__ == '__main__':
